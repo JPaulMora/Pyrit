8b83420e51a228fb92ba43e07ac8ac757568944e
knabberknusperhaus <knabberknusperhaus@yahoo.de>
2008-09-06 12:31:03 +0000

    Initial commit

--- cpyrit/cpyrit.py
+++ cpyrit/cpyrit.py
@@ -1,8 +1,28 @@
+# -*- coding: UTF-8 -*-
+
+
+#    This file is part of Pyrit.
+#
+#    Pyrit is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Pyrit is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Pyrit.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import _cpyrit
 import threading
 import time
 
 class MicroCore(threading.Thread):
+""" Used by GPU-kernels to spread work between CPU and GPU"""
     def __init__(self, func, blocksize, workcontrol):
         threading.Thread.__init__(self)
         self.workcontrol = workcontrol
