4940b3cd2818619bb73b2da65cd9bbb2f4a122ff
knabberknusperhaus <knabberknusperhaus@yahoo.de>
2009-03-18 21:44:44 +0000

    Moved _cpyrit_cuda from Runtime-API to Driver-API.

--- cpyrit_cuda/_cpyrit_cuda.c
+++ cpyrit_cuda/_cpyrit_cuda.c
@@ -18,61 +18,164 @@
 #    along with Pyrit.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <Python.h>
+#include <openssl/hmac.h>
+#include <openssl/sha.h>
 #include "_cpyrit_cuda.h"
 
-int cudaDevCount;
-cudaDevContext *cudaDevices;
+// Created by NVCC and setup.py
+#include "_cpyrit_cudakernel.cubin.h"
 
-//Will be compiled by nvcc and linked in
-int calc_pmklist(gpu_inbuffer *inbuffer, gpu_outbuffer *outbuffer, int buffersize);
+static PyTypeObject CUDADevice_type;
+
+typedef struct
+{
+    PyObject_HEAD
+    int dev_idx;
+    char dev_name[64];
+    CUmodule mod;
+    CUfunction kernel;
+    CUcontext dev_ctx;
+} CUDADevice;
+
+int cudaDevCount;
+
+static int
+cudadev_init(CUDADevice *self, PyObject *args, PyObject *kwds)
+{
+    int dev_idx;
+
+	if (!PyArg_ParseTuple(args, "i:CUDADevice", &dev_idx))
+		return -1;
+
+    if (dev_idx < 0 || dev_idx > cudaDevCount-1)
+    {
+        PyErr_SetString(PyExc_SystemError, "Invalid device number");
+        return -1;
+    }
+    self->dev_idx = dev_idx;
+    
+    if (cuDeviceGetName(self->dev_name, sizeof(self->dev_name), self->dev_idx) != CUDA_SUCCESS)
+    {
+        PyErr_SetString(PyExc_SystemError, "Failed to resolve device name.");
+        return -1;
+    }
+    
+    if (cuCtxCreate(&self->dev_ctx, CU_CTX_SCHED_YIELD, self->dev_idx) != CUDA_SUCCESS)
+    {
+        PyErr_SetString(PyExc_SystemError, "Failed to create device-context.");
+        return -1;
+    }
+    
+    if (cuModuleLoadData(&self->mod, &__cudakernel_module) != CUDA_SUCCESS)
+    {
+        PyErr_SetString(PyExc_SystemError, "Failed to load CUBIN-module.");
+        return -1;
+    }
+
+    if (cuModuleGetFunction(&self->kernel, self->mod, "cuda_pmk_kernel") != CUDA_SUCCESS)
+    {
+        PyErr_SetString(PyExc_SystemError, "Failed to load kernel-function");
+        return -1;    
+    }
+    
+    if (cuFuncSetBlockShape(self->kernel, THREADS_PER_BLOCK, 1, 1) != CUDA_SUCCESS)
+    {
+        PyErr_SetString(PyExc_SystemError, "Failed to set block-shape");
+        return -1;    
+    }
+    
+    if (cuCtxPopCurrent(NULL) != CUDA_SUCCESS)
+    {
+        cuCtxDestroy(self->dev_ctx);
+        PyErr_SetString(PyExc_SystemError, "Failed to detach from device-context after creation.");        
+        return -1;
+    }
+
+    return 0;
+}
+
+static void
+cudadev_dealloc(CUDADevice *self)
+{
+    cuModuleUnload(self->mod);
+    cuCtxDestroy(self->dev_ctx);
+    PyObject_Del(self);
+}
 
 PyObject* cpyrit_listDevices(PyObject* self, PyObject* args)
 {
     int i;
     PyObject* result;
+    char dev_name[64];
+    
     if (!PyArg_ParseTuple(args, "")) return NULL;
     
     result = PyTuple_New(cudaDevCount);
     for (i = 0; i < cudaDevCount; i++)
     {
-        PyTuple_SetItem(result, i, Py_BuildValue("(siiii)", &cudaDevices[i].properties.name,
-                                                cudaDevices[i].properties.totalGlobalMem, cudaDevices[i].properties.regsPerBlock, 
-                                                cudaDevices[i].properties.clockRate, cudaDevices[i].properties.multiProcessorCount));
+        cuDeviceGetName(dev_name, sizeof(dev_name), i);
+        PyTuple_SetItem(result, i, Py_BuildValue("(s)", &dev_name));
     }
 
     return result;
 }
 
-// Sets CUDA-device for the current thread.
-PyObject* cpyrit_setDevice(PyObject* self, PyObject* args)
+CUresult calc_pmklist(CUDADevice *self, gpu_inbuffer *inbuffer, gpu_outbuffer* outbuffer, int size)
 {
-    int cudaDev, cudaDev_new;
-    if (!PyArg_ParseTuple(args, "i", &cudaDev_new)) return NULL;
-
-    if (cudaDev_new < 0 || cudaDev_new > cudaDevCount-1)
+    CUdeviceptr g_inbuffer, g_outbuffer;
+    CUresult ret;
+    int buffersize;
+    
+    // Align size of memory allocation and operations to full threadblocks. Threadblocks should be aligned to warp-size.
+    buffersize = (size / THREADS_PER_BLOCK + (size % THREADS_PER_BLOCK == 0 ? 0 : 1)) * THREADS_PER_BLOCK;
+    ret = cuMemAlloc(&g_inbuffer, buffersize*sizeof(gpu_inbuffer));
+    if (ret != CUDA_SUCCESS)
+        return ret;
+    
+    ret = cuMemAlloc(&g_outbuffer, buffersize*sizeof(gpu_outbuffer));
+    if (ret != CUDA_SUCCESS)
     {
-        PyErr_SetString(PyExc_SystemError, "Invalid device number");
-        return NULL;
+        cuMemFree(g_inbuffer);
+        return ret;
+    }
+    
+    ret = cuMemcpyHtoD(g_inbuffer, inbuffer, size*sizeof(gpu_inbuffer));
+    if (ret != CUDA_SUCCESS)
+    {
+        cuMemFree(g_outbuffer);
+        cuMemFree(g_inbuffer);
+        return ret;
+    }
+    
+    cuParamSeti(self->kernel, 0, g_inbuffer);
+    cuParamSeti(self->kernel, sizeof(void*), g_outbuffer);
+    cuParamSetSize(self->kernel, sizeof(void*)*2);
+    ret = cuLaunchGrid(self->kernel, buffersize / THREADS_PER_BLOCK, 1);
+    if (ret != CUDA_SUCCESS)
+    {
+        cuMemFree(g_inbuffer);
+        cuMemFree(g_outbuffer);
+        cuCtxPopCurrent(NULL);
+        return ret;
     }
 
-    if (cudaSetDevice(cudaDev_new) != cudaSuccess)
-    {
-        if (cudaGetDevice(&cudaDev) != cudaSuccess)
-        {
-            PyErr_SetString(PyExc_SystemError, "Failed to get/set new device.");
-            return NULL;
-        }
-        if (cudaDev_new != cudaDev)
-        {
-            PyErr_SetString(PyExc_SystemError, "Failed to set new device.");
-            return NULL;
-        }
-    }
+    cuMemFree(g_inbuffer);
 
-    return Py_BuildValue("i", cudaDev_new);
+    ret = cuMemcpyDtoH(outbuffer, g_outbuffer, size*sizeof(gpu_outbuffer));
+    if (ret != CUDA_SUCCESS)
+    {
+        cuMemFree(g_outbuffer);
+        cuCtxPopCurrent(NULL);
+        return ret;
+    }
+    
+    cuMemFree(g_outbuffer);
+
+    return CUDA_SUCCESS;
 }
 
-PyObject *cpyrit_pmklist(PyObject *self, PyObject *args)
+PyObject *cpyrit_pmklist(CUDADevice *self, PyObject *args)
 {
     char *essid_pre, *key, essid[33+4];
     unsigned char pad[64], temp[32];
@@ -135,17 +238,25 @@ PyObject *cpyrit_pmklist(PyObject *self, PyObject *args)
         GET_BE(c_inbuffer[i].e2.h2, temp, 8); GET_BE(c_inbuffer[i].e2.h3, temp, 12);
         GET_BE(c_inbuffer[i].e2.h4, temp, 16);
     }
+    
+    if (cuCtxPushCurrent(self->dev_ctx) != CUDA_SUCCESS)
+    {
+        PyErr_SetString(PyExc_SystemError, "Failed to attach to device-context before launch.");
+        free(c_inbuffer);
+        free(c_outbuffer);
+        return NULL;
+    }
 
     Py_BEGIN_ALLOW_THREADS;
-    ret = calc_pmklist(c_inbuffer, c_outbuffer, numLines);
+    ret = calc_pmklist(self, c_inbuffer, c_outbuffer, numLines);
     Py_END_ALLOW_THREADS;
     
+    cuCtxPopCurrent(NULL);
     free(c_inbuffer);
-
-    if (ret != cudaSuccess)
+    if (ret != CUDA_SUCCESS)
     {
-        PyErr_SetString(PyExc_IOError, cudaGetErrorString(ret));
         free(c_outbuffer);
+        PyErr_SetString(PyExc_SystemError, "Failed to launch CUDA-kernel.");
         return NULL;
     }
 
@@ -164,48 +275,79 @@ PyObject *cpyrit_pmklist(PyObject *self, PyObject *args)
     return resultList;
 }
 
-static PyMethodDef CPyritCUDAMethods[] = {
-    {"calc_pmklist", cpyrit_pmklist, METH_VARARGS, "Calculate PMKs from ESSID and list of strings"},
-    {"setDevice", cpyrit_setDevice, METH_VARARGS, "Binds the current thread to the given device. Can only be called once per thread."},
-    {"listDevices", cpyrit_listDevices, METH_VARARGS, "Returns a tuple to tuples, each describing a CUDA-capable device."},
+
+static PyMethodDef CUDADevice_methods[] =
+{
+    {"calc_pmklist", (PyObject*)cpyrit_pmklist, METH_VARARGS, "Calculate PMKs from ESSID and list of strings."},
+    {NULL, NULL}
+};
+
+static PyTypeObject CUDADevice_type = {
+	PyObject_HEAD_INIT(NULL)
+	0,			/*ob_size*/
+	"_cpyrit_cuda.CUDADevice",	/*tp_name*/
+	sizeof(CUDADevice), /*tp_basicsize*/
+	0,			/*tp_itemsize*/
+	(destructor)cudadev_dealloc, /*tp_dealloc*/
+	0,			/*tp_print*/
+	0,			/*tp_getattr*/
+	0,			/*tp_setattr*/
+	0,			/*tp_compare*/
+	0,			/*tp_repr*/
+	0,			/*tp_as_number*/
+	0,			/*tp_as_sequence*/
+	0,			/*tp_as_mapping*/
+	0,			/*tp_hash*/
+    0,                      /*tp_call*/
+    0,                      /*tp_str*/
+    PyObject_GenericGetAttr,/*tp_getattro*/
+    PyObject_GenericSetAttr,/*tp_setattro*/
+    0,                      /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,      /*tp_flags*/
+    0,       /*tp_doc*/
+    0,                      /*tp_traverse*/
+    0,                      /*tp_clear*/
+    0,                      /*tp_richcompare*/
+    0,                      /*tp_weaklistoffset*/
+    0,                      /*tp_iter*/
+    0,                      /*tp_iternext*/
+    CUDADevice_methods,      /*tp_methods*/
+    0,      /*tp_members*/
+    0,                      /*tp_getset*/
+    0,                      /*tp_base*/
+    0,                      /*tp_dict*/
+    0,                      /*tp_descr_get*/
+    0,                      /*tp_descr_set*/
+    0,                      /*tp_dictoffset*/
+    (initproc)cudadev_init, /*tp_init*/
+    PyType_GenericAlloc,    /*tp_alloc*/
+    PyType_GenericNew,      /*tp_new*/
+  	_PyObject_Del,          /*tp_free*/
+    0,                      /*tp_is_gc*/
+};
+
+
+static PyMethodDef CPyritCUDA_methods[] = {
+    {"listDevices", cpyrit_listDevices, METH_VARARGS, "Returns a tuple of tuples, each describing a CUDA-capable device."},
     {NULL, NULL, 0, NULL}
 };
 
 PyMODINIT_FUNC
 init_cpyrit_cuda(void)
 {
-    int ret, i;
-
-    ret = cudaGetDeviceCount(&cudaDevCount);
-    if (ret != cudaSuccess || cudaDevCount < 1)
+    PyObject *m;
+    
+    if (cuInit(0) != CUDA_SUCCESS || cuDeviceGetCount(&cudaDevCount) != CUDA_SUCCESS || cudaDevCount < 1)
     {
         PyErr_SetString(PyExc_ImportError, "CUDA seems to be unavailable or no device reported.");
         return;
     }
+
+    if (PyType_Ready(&CUDADevice_type) < 0)
+	    return;
+
+    m = Py_InitModule("_cpyrit_cuda", CPyritCUDA_methods);
     
-    cudaDevices = malloc(cudaDevCount * (sizeof(cudaDevContext)));
-    for (i = 0; i < cudaDevCount; i++)
-    {
-        cudaGetDeviceProperties(&cudaDevices[i].properties, i);
-        cudaDevices[i].device = i;
-    }
-    
-    ret = cudaGetLastError();
-    if (ret != cudaSuccess)
-    {
-        PyErr_SetString(PyExc_ImportError, cudaGetErrorString(ret));
-        return;
-    }
-
-    (void) Py_InitModule("_cpyrit_cuda", CPyritCUDAMethods);
-}
-
-int
-main(int argc, char *argv[])
-{
-    Py_Initialize();
-
-    init_cpyrit_cuda();
-
-    return -1;
+    Py_INCREF(&CUDADevice_type);
+    PyModule_AddObject(m, "CUDADevice", (PyObject *)&CUDADevice_type);
 }
