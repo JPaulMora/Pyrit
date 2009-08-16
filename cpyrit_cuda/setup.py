9e8db26e730a3b9c0463c78f4eb1c25fb6f54dc1
lukas.lueg <lukas.lueg@a34e5296-3756-0410-aa27-512c484a3442>
2009-08-16 11:19:42 +0000

    Tagged version 0.2.4

--- cpyrit_cuda/setup.py
+++ cpyrit_cuda/setup.py
@@ -41,9 +41,9 @@ else:
 
 try:
     svn_info = subprocess.Popen(('svn', 'info'), stdout=subprocess.PIPE).stdout.read()
-    version_string = '0.2.4-dev (svn r%i)' % int(re.compile('Revision: ([0-9]*)').findall(svn_info)[0])
+    version_string = '0.2.4 (svn r%i)' % int(re.compile('Revision: ([0-9]*)').findall(svn_info)[0])
 except:
-    version_string = '0.2.4-dev'
+    version_string = '0.2.4'
 EXTRA_COMPILE_ARGS = ['-DVERSION="%s"' % version_string]
 
 
